f7dd75152ba4b3fc8fbdf7fa279b9ed0e0968583
clarkgh <demolisho@hotmail.com>
2017-12-06 15:08:49 -0800

    React added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,17 @@
-console.log('I am born');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+);
