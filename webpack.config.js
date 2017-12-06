53ef0be236a4a81e25f99030c3938fa28453c362
clarkgh <demolisho@hotmail.com>
2017-12-06 14:57:32 -0800

    webpack and babel config set

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
-const path = require('path');
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Constant with our paths
+// Path Constants
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
-  JS: path.resolve(__dirname, 'src/scripts'),
+  SCRIPTS: path.resolve(__dirname, 'src/scripts'),
+  SRC: path.resolve(__dirname, 'src'),
 };
 
 // Webpack configuration
 module.exports = {
-  entry: path.join(paths.JS, 'app.js'),
+  entry: path.join(paths.SCRIPTS, 'app.js'),
   output: {
     path: paths.DIST,
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+  // Loaders
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
 };
