8d50da16aa0de31a3071c22d779533fd02cfd75b
clarkgh <demolisho@hotmail.com>
2017-12-06 13:53:48 -0800

    new file layout for babel/webpack

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express'),
   app = express(),
   path = require('path');
 
-app.use(express.static('./'))
+app.use(express.static('./src/'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 app.listen(8080, () => {
