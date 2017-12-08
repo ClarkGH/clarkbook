777123354762e2bfbc2ee1e29fffe9d75e42231a
clarkgh <demolisho@hotmail.com>
2017-12-08 12:48:53 -0800

    Added emotionjs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled, { css, injectGlobal } from 'react-emotion';
+
+
+const Container = styled('div')`
+  background: #333;
+`
+// const myStyle = css`
+//   color: rebeccapurple;
+// `
+
 ReactDOM.render(
-  <div>Hi</div>,
+  <Container>Hi</Container>,
   document.getElementById('root')
 );
