9b49f015307d2b3b6e67aa43f8277121b91331a1
clarkgh <demolisho@hotmail.com>
2017-12-14 14:54:03 -0800

    removed babel components and tested sans babel. Loads fine, reconfiguring babel setup.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,30 +1,32 @@
 "use strict";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import styled, { css, injectGlobal } from "react-emotion";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+console.log("I live again");
 
-// Global Styles
-injectGlobal`
-html,body{
-  margin: 0;
-  padding: 0;
-  width: 100%;
-}
-`;
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import styled, { css, injectGlobal } from "react-emotion";
+// import { Header } from "./components/Header";
+// import { Footer } from "./components/Footer";
 
-// Specific Container styles
-const Container = styled("div")``; //styles in the backticks
-// Component className styles
-const mainStyles = css``; //styles in the backticks
+// // Global Styles
+// injectGlobal`
+// html,body{
+//   margin: 0;
+//   padding: 0;
+//   width: 100%;
+// }
+// `;
 
-ReactDOM.render(
-  <Container>
-    <Header />
-    <div>Body stuff here (will need if-else logic)</div>
-    <Footer />
-  </Container>,
-  document.getElementById("root")
-);
+// // Specific Container styles
+// const Container = styled("div")``; //styles in the backticks
+// // Component className styles
+// const mainStyles = css``; //styles in the backticks
+
+// ReactDOM.render(
+//   <Container>
+//     <Header />
+//     <div>Body stuff here (will need if-else logic)</div>
+//     <Footer />
+//   </Container>,
+//   document.getElementById("root")
+// );
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+// Path Constants
+const paths = {
+  DIST: path.resolve(__dirname, "dist"),
+  SCRIPTS: path.resolve(__dirname, "src/scripts"),
+  SRC: path.resolve(__dirname, "src")
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.SCRIPTS, "app.js"),
+  output: {
+    path: paths.DIST,
+    filename: "app.bundle.js"
+  },
+  // Loaders
+  module: {
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
