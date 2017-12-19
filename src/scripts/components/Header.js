d244bb9abc4cbf6bd7a2f6e2343cf2545665438b
clarkgh <demolisho@hotmail.com>
2017-12-19 14:50:02 -0800

    content for header added, next is styling

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -3,9 +3,21 @@ import React from 'react';
 export class Header extends React.Component {
   render() {
     return(
-      <header>
-        <h1>Navbar</h1>
-      </header>
+      <nav>
+        <h1 className="logo">Logo</h1>
+        <form>
+          <label>
+            Email:
+            <input type="email" name="email" />
+          </label>
+          <label>
+            Password:
+            <input type="password" minLength="8" maxLength="32"/>
+          </label>
+          <a href="#">Forgot Password?</a>
+          <input type="submit" value="Submit"/>
+        </form>
+      </nav>
     );
   }
 }
